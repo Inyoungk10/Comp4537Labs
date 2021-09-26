cd5d43c692bd26d7033949aff9fb06318ed5c42e
inyoungk10 <inyoungk10@gmail.com>
2021-09-26 09:30:32 -0700

    object constructor

--- COMP4537/labs/1/js/reader.js
+++ COMP4537/labs/1/js/reader.js
@@ -1,5 +1,7 @@
 const msg_notSupported = "Sorry web Storage is not supported!";
+
 let textArray = [];
+localStorage.clear();
 
 // reading and updating time every 2 seconds
 if (typeof (Storage) == "undefined") {
@@ -8,7 +10,6 @@ if (typeof (Storage) == "undefined") {
 }
 
 window.setInterval(function() {
-
     let div = document.getElementById("notesReader");
     div.innerHTML = "";
         
@@ -17,20 +18,20 @@ window.setInterval(function() {
     document.getElementById("timeR").innerHTML = "updated at: " + time.toLocaleString('en-US', { hour: 'numeric', minute: 'numeric', second: 'numeric', hour12: true});
 
     textArray = localStorage.getItem("array");
-    let split = textArray.split(",");
 
+    if (textArray != null) {
+        let split = textArray.split(",");
+
+        for (j = 0; j < split.length; j++) {
+            let n = new note(split[j], div);
+        }
+    }
     console.log(textArray);
 
-    // reading
-    for (j = 0; j < split.length; j++) {
-        
-
-        // create p tag
-        let ptag= document.createElement("p");
-        ptag.innerHTML = split[j];
-        // append p tag to div
-        
-        div.appendChild(ptag);
-    }
-
 }, 2000);
+
+function note(text, parentDiv) {
+    this.note = document.createElement("p");
+    this.note.innerHTML = text;
+    parentDiv.appendChild(this.note);
+}
--- COMP4537/labs/1/js/writer.js
+++ COMP4537/labs/1/js/writer.js
@@ -15,42 +15,53 @@ window.setInterval(function() {
     let time = new Date();
     document.getElementById("timeW").innerHTML = "stored at: " + time.toLocaleString('en-US', { hour: 'numeric', minute: 'numeric', second: 'numeric', hour12: true});
 
-    let textArray = []
+    let textArray = [];
 
     for (i = 0; i < count; i++) {
         let textBox = document.getElementById("text" + i);
+        if (textBox == null) {
+            continue;
+        }
+        console.log(textBox.value);
         let text = textBox.value;
         textArray.push(text);
+        //console.log("text being pushed: " + text);
         localStorage.setItem("array", textArray);
     };
-
-    console.log(storage);
     
 }, 2000);
+
+function txtbox(id, type, bgcolor, textcolor, parentDiv) {
+    this.tb = document.createElement("input");
+    this.tb.id = "text" + id;
+    this.type = type;
+    this.tb.style.background = bgcolor;
+    this.tb.style.color = textcolor;
+    parentDiv.appendChild(this.tb);
+}
+
+function rButton(bgcolor, txtcolor, id, parentDiv) {
+    this.btn = document.createElement("button");
+    this.btn.innerHTML = "remove";
+    this.btn.style.backgroundColor = bgcolor;
+    this.btn.style.color = txtcolor;
+    this.btn.id = "btn" + id;
+    this.btn.onclick = function () {
+        document.getElementById("text" + id).remove();
+        document.getElementById("btn" + id).remove();
+    };
+    parentDiv.appendChild(this.btn);
+}
 
 document.getElementById("add").onclick = function() {
     let notesDiv = document.getElementById("notes");
 
     let div = document.createElement("div");
-    
-    let textbox = document.createElement("input");
-    textbox.setAttribute("type", "text");
-    textbox.setAttribute("style", "background-color: blue; color: white;");
-    textbox.setAttribute("id", "text" + count);
-    div.appendChild(textbox);
-
-    let removeButton = document.createElement("button");
-    removeButton.innerHTML = "remove";
-    removeButton.setAttribute("style", "background-color:orange; color: white;");
-    removeButton.onclick = function() {
-        div.removeChild(textbox);
-        div.removeChild(removeButton);
-    }
-
-    div.appendChild(removeButton);
+    let textbox = new txtbox(count, "text", "blue", "white", div);
+    let removeButton = new rButton("orange", "white", count, div);
 
     notesDiv.appendChild(div);
-
+    console.log("yay!" + count);
     count++;
-    console.log("yay!" + textbox.id);
+    
 };
