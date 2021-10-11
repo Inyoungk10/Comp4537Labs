56e85905cc45349cf8e0ed496d547d6a7593b498
inyoungk10 <inyoungk10@gmail.com>
2021-10-10 21:51:42 -0700

    server.js

---
+++ COMP4537/server.js
@@ -0,0 +1,16 @@
+let http = require('http');
+let url = require('url');
+
+const util = require('./labs/3/getDate/modules/utils');
+
+http.createServer(function (req, res) {
+    let q = url.parse(req.url, true);
+    console.log(q.query);
+    res.writeHead(200, {'Content-type' : 'text/html'});
+
+    res.end('<h2 style="color:blue">  Hello ' + q.query["name"] + ` What a beautiful day. Server current date and time is ${utils.getCurrent()} </h2>`);
+
+}).listen(8000);
+
+//http://localhost:8000/?name=Peter
+console.log('listening....');
