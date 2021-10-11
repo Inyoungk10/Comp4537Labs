c46e8359463ddececb00aa8ea192163b7ae65cdc
inyoungk10 <inyoungk10@gmail.com>
2021-10-10 21:49:41 -0700

    lab 3 path

---
+++ COMP4537/labs/3/app.js
@@ -0,0 +1,12 @@
+const math = require('./modules/math');
+
+console.log(`Hello INYOUNG and FRANCIS.`);
+console.log(`1 + 2 = ${math.add(1, 2)}`);
+console.log(`10 - 4 = ${math.subtract(10, 4)}`);
+
+
+
+
+
+
+
